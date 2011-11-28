a030fa80b30683b230b5eba5e36d2c1f59137910
Jason Michels <thebizztech@me.com>
2011-11-28 09:20:35 -0600

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Test Credit Card Generator [Jason Michels] (http://thebizztech.com/)
+
+This is a test credit card generator I use to test PayPal and PayFlow transactions.
---
+++ generate_cc.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<title>Generate Credit Cards</title>
+<script language="JavaScript">
+	function generateCC(){
+		var cc_number = new Array(16);
+		var cc_len = 16;
+		var start = 0;
+		var rand_number = Math.random();
+
+		switch(document.cc_generator.creditCardType.value)
+        {
+			case "Visa":
+				cc_number[start++] = 4;
+				break;
+			case "Discover":
+				cc_number[start++] = 6;
+				cc_number[start++] = 0;
+				cc_number[start++] = 1;
+				cc_number[start++] = 1;
+				break;
+			case "MasterCard":
+				cc_number[start++] = 5;
+				cc_number[start++] = Math.floor(Math.random() * 5) + 1;
+				break;
+			case "Amex":
+				cc_number[start++] = 3;
+				cc_number[start++] = Math.round(Math.random()) ? 7 : 4 ;
+				cc_len = 15;
+				break;
+        }
+
+        for (var i = start; i < (cc_len - 1); i++) {
+			cc_number[i] = Math.floor(Math.random() * 10);
+        }
+
+		var sum = 0;
+		for (var j = 0; j < (cc_len - 1); j++) {
+			var digit = cc_number[j];
+			if ((j & 1) == (cc_len & 1)) digit *= 2;
+			if (digit > 9) digit -= 9;
+			sum += digit;
+		}
+
+		var check_digit = new Array(0, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+		cc_number[cc_len - 1] = check_digit[sum % 10];
+
+		document.cc_generator.creditCardNumber.value = "";
+		for (var k = 0; k < cc_len; k++) {
+			document.cc_generator.creditCardNumber.value += cc_number[k];
+		}
+	}
+</script>
+</head>
+
+<body alink=#0000FF vlink=#0000FF>
+
+<br>
+<center>
+<font size=2 color=black face=Verdana><b>Credit Card Generator</b></font>
+<br><br>
+<form method="POST" action="#" name="cc_generator">
+<table width=600>
+	<tr>
+		<td align=right>Card Type:</td>
+		<td align=left>
+			<select name=creditCardType onChange="javascript:generateCC(); return false;">
+				<option value=Visa selected>Visa</option>
+				<option value=MasterCard>MasterCard</option>
+				<option value=Discover>Discover</option>
+				<option value=Amex>American Express</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td align=right>Card Number:</td>
+		<td align=left><input type=text size=19 maxlength=19 name=creditCardNumber></td>
+	</tr>
+</table>
+</form>
+</center>
+<script language="javascript">
+	generateCC();
+</script>
+</body>
+</html>
